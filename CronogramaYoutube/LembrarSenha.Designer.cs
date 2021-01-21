44d9ed9d842de6158ee1084c82ddc126a4c66c55
Gabriel Poppes <gabrielpoppes@outlook.com>
2021-01-20 23:45:00 -0300

    Adicionado nova tela de recuperação de senha, e feito o layout da mesma

---
+++ CronogramaYoutube/LembrarSenha.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace CronogramaYoutube
+{
+    partial class LembrarSenha
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LembrarSenha));
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtBox_email = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_recuperarsenha = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Brown;
+            this.button1.Location = new System.Drawing.Point(-99, -7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(956, 136);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Brown;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(75, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(644, 99);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtBox_email
+            // 
+            this.txtBox_email.Location = new System.Drawing.Point(304, 228);
+            this.txtBox_email.Name = "txtBox_email";
+            this.txtBox_email.Size = new System.Drawing.Size(187, 20);
+            this.txtBox_email.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(369, 206);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "E-MAIL";
+            // 
+            // btn_recuperarsenha
+            // 
+            this.btn_recuperarsenha.BackColor = System.Drawing.Color.Firebrick;
+            this.btn_recuperarsenha.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_recuperarsenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_recuperarsenha.Location = new System.Drawing.Point(304, 254);
+            this.btn_recuperarsenha.Name = "btn_recuperarsenha";
+            this.btn_recuperarsenha.Size = new System.Drawing.Size(187, 39);
+            this.btn_recuperarsenha.TabIndex = 5;
+            this.btn_recuperarsenha.Text = "RECUPERAR SENHA";
+            this.btn_recuperarsenha.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(172, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(470, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Para recuperar a senha do sistema, digite o e-mail cadastrado no seu usuário";
+            // 
+            // LembrarSenha
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.IndianRed;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_recuperarsenha);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBox_email);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "LembrarSenha";
+            this.Text = "LembrarSenha";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtBox_email;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_recuperarsenha;
+        private System.Windows.Forms.Label label2;
+    }
+}
